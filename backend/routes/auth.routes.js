965827967836cbb8842968c337bbf5036063b9a8
Satyamsahu20 <132657411+Satyamsahu20@users.noreply.github.com>
2026-01-10 12:58:51 +0530

    Small Changes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express"
-import { googleAuth, resetPassword, sendOtp, signIn, signOut, signUp, verifyOtp } from "../controllers/auth.controllers.js"
+// import { googleAuth, resetPassword, sendOtp, signIn, signOut, signUp, verifyOtp } from "../controllers/auth.controllers.js"
+import {resetPassword, sendOtp, signIn, signOut, signUp, verifyOtp } from "../controllers/auth.controllers.js"
 
 const authRouter=express.Router()
 
@@ -9,6 +10,6 @@ authRouter.get("/signout",signOut)
 authRouter.post("/send-otp",sendOtp)
 authRouter.post("/verify-otp",verifyOtp)
 authRouter.post("/reset-password",resetPassword)
-authRouter.post("/google-auth",googleAuth)
+// authRouter.post("/google-auth",googleAuth)
 
 export default authRouter
